ac885732121ca7977434fc792e30b59ba2a152b4
MateusC <mateus.martino@gmail.com>
2014-05-14 10:21:04 -0300

    .

--- MateusRepositorio/Unidade_8/Gerador_Ingresso.cs
+++ MateusRepositorio/Unidade_8/Gerador_Ingresso.cs
@@ -13,7 +13,7 @@ namespace Unidade_8
             Random gerador = new Random();
             int setor, cadeira = 0;
             setor = gerador.Next(1,5);
-            cadeira = gerador.Next(1,201);
+            cadeira = gerador.Next(1,21);
             Console.WriteLine("Setor {0}, Cadeira {1}",setor,cadeira);
             Console.ReadKey();
 
