a045e1b6cad1ffbc46cf2c8e7a77c00c0be707a8
Matt <mateus.martino@gmail.com>
2014-06-02 17:42:23 -0300

    Comitando exercicios unidade 10

---
+++ MateusRepositorio/Unidade_10/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicios em ordem 
+
+            //ConsumoDeCombustivel();
+            //JurosComposto();
+            //IRPF();
+            //IMC();
+            //ArrayAleatorio();
+        }
+        static void ConsumoDeCombustivel()
+        {
+
+            Console.WriteLine("Distancia percorrida : ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade de Combustivel : ");
+            double y = double.Parse(Console.ReadLine());
+            double resp = x / y;
+            Console.WriteLine("É gasto {0} litros de gasolina por km ",resp);
+            Console.ReadKey();
+        }
+        static void JurosComposto()
+        {
+            Console.Write("Quantia pega : ");
+            double valor = double.Parse(Console.ReadLine());
+            Console.Write("Taxa de juros : ");
+            double taxa = double.Parse(Console.ReadLine());
+            taxa = taxa/100;
+            Console.Write("Tempo de pagamento : ");
+            int tempo = int.Parse(Console.ReadLine());
+            double potencia = Math.Pow(taxa+1,tempo);
+            double juros = valor * potencia;
+            Console.WriteLine("Voce pagara no total : {0:F2}", juros);
+            Console.ReadKey();
+        }
+        static void IRPF()
+        {
+            //Base de cálculo anual em R$	Alíquota (%)
+            //Até 1.637,11	                     -	                
+            //De 1.637,12 até 2.453,50	        7,5	               
+            //De 2.453,51 até 3.271,38	        15	               
+            //De 3.271,39 até 4.087,65	        22,5	           
+            //Acima de 4.087,65	                27,5	            
+            double desconto;
+            Console.Write("Digite o salário : ");
+            double Salario = double.Parse(Console.ReadLine());
+            if (Salario > 4087.85)
+            {
+                desconto = (Salario * 27.5) / 100;
+            }
+            else if (Salario > 3271.38)
+            {
+                desconto = (Salario * 22.5) / 100;
+            }
+            else if (Salario > 2453.50)
+            {
+                desconto = (Salario * 15) / 100;
+            }
+            else if (Salario > 1637.11)
+            {
+                desconto = (Salario * 7.5) / 100;
+            }
+            else
+            {
+                desconto = 0;
+            }
+            Console.Write("Seu imposto ficou em {0:f2} R$ por receber {1} R$", desconto, Salario);
+            Console.ReadKey();
+        }
+        static void IMC()
+        {
+            //Abaixo de 17	Muito abaixo do peso
+            //Entre 17 e 18,49	Abaixo do peso
+            //Entre 18,5 e 24,99	Peso normal
+            //Entre 25 e 29,99	Acima do peso
+            //Entre 30 e 34,99	Obesidade I
+            //Entre 35 e 39,99	Obesidade II (severa)
+            //Acima de 40	Obesidade III (mórbida)
+            string situacao =null;
+            Console.Write("Peso :");
+            double peso = double.Parse(Console.ReadLine());
+            Console.Write("Altura :");
+            double altura = double.Parse(Console.ReadLine());
+            double imc = peso / Math.Pow(altura, 2);
+            if (imc > 40)
+            {
+                situacao = "Obesidade III (mórbida)";
+            }
+            else if (imc >= 35)
+            {
+                situacao = "Obesidade II (severa)";
+            }
+            else if (imc >= 30)
+            {
+                situacao = "Obesidade I";
+            }
+            else if (imc >= 25)
+            {
+                situacao = "Acima do peso";
+            }
+            else if (imc >= 18.5)
+            {
+                situacao = "Peso normal";
+            }
+            else if (imc >= 17)
+            {
+                situacao = "Abaixo do peso";
+            }
+            else if (imc < 17)
+            {
+                situacao = "Muito abaixo do peso";
+            }
+            Console.WriteLine("Situacao :{0}\nIMC : {1:f2}",situacao,imc);
+            Console.ReadKey();
+        }
+        static void ArrayAleatorio()
+        {
+            Random gerador = new Random();
+            int sorteio = 0;
+            int[] array = new int[20];
+            int[] posicao = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                sorteio = gerador.Next(0, 100);
+                array[i] = sorteio;
+                posicao[i] = i;
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("{0} : {1}", posicao[i],array[i] );
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MateusRepositorio/Unidade_10_Complementar/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_10_Complementar
+{
+    class Program
+    {
+        static int[] idade = new int[20];
+        static int maiorIdade = 0;
+        static int menorIdade = 200;
+        static double mediaIdade = 0;
+        static Random idades = new Random();
+        static int op = 0;
+        static void Main(string[] args)
+        {
+            
+            //Chamar os metodos em ordem
+
+            //AnoBissexto();
+            //ValidarData();
+            //Idades();
+            //InicioTabuada();
+        }
+        static void AnoBissexto()
+        {
+            Console.Write("Ano : ");
+            int ano = int.Parse(Console.ReadLine());
+            if ( ano % 4 == 0 && ano % 100 !=0)
+            {
+                Console.WriteLine("Ano bissexto.");
+            }
+            else if (ano % 400 == 0 && ano % 4 == 0) 
+            {
+                Console.WriteLine("Ano bissexto.");
+            }
+            else
+            {
+                Console.WriteLine("Nao bissexto.");
+            }
+            Console.ReadKey();
+        }
+        static void ValidarData()
+        {
+            int ano,dia,mes=0;
+            int maxDias=0;
+            int bissex =0;
+            string mes1 = null;
+            do{
+                Console.Write("Ano: ");
+                ano = int.Parse(Console.ReadLine());
+                if (ano < 0 || ano > 4000)
+                {
+                    Console.WriteLine("Digite o ano valido (0 - 4000)");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (ano < 0 || ano > 4000);
+            if ( ano % 4 == 0 && ano % 100 !=0)
+            {
+                bissex= 1;
+            }
+            else if (ano % 400 == 0 && ano % 4 == 0) 
+            {
+                bissex= 1;
+            }
+            else
+            {
+                bissex= 0;
+            }
+            do{
+                Console.Write("Mes :");
+                mes = int.Parse(Console.ReadLine());
+                if (mes < 1 || mes > 12)
+                {
+                    Console.WriteLine("Digite um mes valido (1 - 12)");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                switch(mes){
+                       case 1:
+                           maxDias =31;
+                           mes1 = "janeiro";
+                           break;
+                       case 2:
+                           maxDias =28+bissex;
+                           mes1 = "fevereiro";
+                           break;
+                       case 3:
+                           maxDias =31;
+                           mes1 = "março";
+                           break;
+                       case 4:
+                           maxDias =30;
+                           mes1 = "abril";
+                           break;
+                       case 5:
+                           maxDias =31;
+                           mes1 = "maio";
+                           break;
+                       case 6:
+                           maxDias =30;
+                           mes1 = "junho";
+                           break;
+                       case 7:
+                           maxDias =31;
+                           mes1 = "julho";
+                           break;
+                       case 8:
+                           maxDias =31;
+                           mes1 = "agosto";
+                           break;
+                       case 9:
+                           maxDias =30;
+                           mes1 = "setembro";
+                           break;
+                       case 10:
+                           maxDias =31;
+                           mes1 = "outubro";
+                           break;
+                       case 11:
+                           maxDias =30;
+                           mes1 = "novembro";
+                           break;
+                       case 12:
+                           maxDias =31;
+                           mes1 = "dezembro";
+                           break;
+                }
+            } while (mes < 1 || mes > 12);
+            do
+            {
+                Console.Write("Dia: ");
+                dia = int.Parse(Console.ReadLine());
+                if (dia < 1 || dia > maxDias)
+                {
+                    Console.WriteLine("Dia incorreto, preencha novamente por favor.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (dia < 1 || dia > maxDias);
+            Console.WriteLine("{0} de {1} de {2}.", dia, mes1, ano);
+            Console.ReadKey();
+        }
+        static void Idades()
+        {
+            CadastrarIdades();
+            MaiorIdade();
+            MenorIdade();
+            MediaIdade();
+            ImprimirIdade();
+        }
+        static void CadastrarIdades()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                idade[i] = idades.Next(0, 100);
+            }
+        }
+        static void MaiorIdade()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (idade[i] > maiorIdade)
+                {
+                    maiorIdade = idade[i];
+                }
+            }
+        }
+        static void MenorIdade()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (idade[i] < menorIdade)
+                {
+                    menorIdade = idade[i];
+                }
+            }
+        }
+        static void MediaIdade()
+        {
+            for (int i = 0; i < 20; i++)
+            {                
+                mediaIdade += idade[i];
+            }
+            mediaIdade /= 20;
+        }
+        static void ImprimirIdade()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Idade {0} = {1}", i, idade[i]);
+            }
+            Console.WriteLine("Maior idade : {0}", maiorIdade);
+            Console.WriteLine("Menor idade : {0}", menorIdade);
+            Console.WriteLine("Media idade : {0:F2}", mediaIdade);
+            Console.ReadKey();
+
+        }
+        static void InicioTabuada()
+        {
+            Console.Write("Informe o numero da tabuada :");
+            op = int.Parse(Console.ReadLine());
+            Tabuada(op);
+        }
+        static void Tabuada(int x)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", x, i + 1, x * (i + 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
